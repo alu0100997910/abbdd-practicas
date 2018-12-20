f20c07d412fcc1feb39466161827440d734969a6
alu0100890709 <32129884+alu0100890709@users.noreply.github.com>
2018-12-20 14:24:06 +0000

    Delete triggerCatastro.sql

--- Triggers/triggerCatastro.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION controlar_habitantes_vivienda() RETURNS TRIGGER AS $actualizar_habitantes_vivienda$
-	BEGIN
-		DELETE FROM ResidePiso WHERE ResidePiso.dni = NEW.dni;
-		RETURN NEW;
-	END;
-$actualizar_habitantes_vivienda$ LANGUAGE plpgsql;
-
-CREATE TRIGGER actualizar_stock_compra AFTER INSERT ON ResideVivienda
-FOR EACH ROW EXECUTE PROCEDURE controlar_habitantes_vivienda();
